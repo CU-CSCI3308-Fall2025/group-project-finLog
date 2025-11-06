1dee8ebbf22d1f09a1b95b9be1ee07037ab44dfc
JuanPleitez <juandipleitez04@gmail.com>
2025-11-06 13:46:50 -0700

    small changes to index.js app.listen

--- src/index.js
+++ src/index.js
@@ -8,6 +8,4 @@ app.get("/welcome", (req, res) => {
   res.json({ status: "success", message: "Welcome!" });
 });
 
-module.exports = app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+module.exports = app.listen(3000)
